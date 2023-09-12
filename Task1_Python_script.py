926b19e9c841b86c24fb8d418a36c252b5648e42
himanisingla0911 <himanisingla603@gmail.com>
2023-09-12 10:46:05 -0400

    All three files are present here

--- Task1_Python_script.py
+++ Task1_Python_script.py
@@ -1,7 +1,9 @@
+# .py file
 # Python script to scrape an article given the url of the article and store the extracted text in a file
 # Url: https://medium.com/@subashgandyer/papa-what-is-a-neural-network-c5e5cc427c7
 
 import os
+
 import requests
 import re
 # Code here - Import BeautifulSoup library
